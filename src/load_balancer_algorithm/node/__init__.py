f51e7e88180be91c0872f12292fb025c8416bf04
Manjusaka <me@manjusaka.me>
2025-03-23 17:53:33 +0800

    Add weight and current_weight attributes to node class
    Signed-off-by: Manjusaka <me@manjusaka.me>

--- src/load_balancer_algorithm/node/__init__.py
+++ src/load_balancer_algorithm/node/__init__.py
@@ -6,7 +6,12 @@ class Node:
     host: str = ""
     port: int = 0
     node_available: bool = True
+    weight = 0
+    current_weight: int = 0
 
     @property
     def available(self) -> bool:
         return self.node_available
+
+    def __post_init__(self):
+        self.current_weight = self.weight
